c4945563af5f2cbb4025655626f47f91267496e2
Guilherme Dambros <guilherme.dambros@gmail.com>
2021-08-25 19:19:33 -0300

    Atribui perfil de operador em todo usuario que se registra

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use App\Models\User;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /**Set default role */
+        User::created(function ($user) {
+            $role = Role::where('name', 'Operador')->first(); 
+            $user->assignRole([$role->id]);
+        });
     }
 }
