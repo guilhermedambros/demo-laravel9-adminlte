eca82928518405d1b3f88d18f97e0d99e248a1b8
Guilherme Dambros <guilherme.dambros@gmail.com>
2021-08-27 09:59:09 -0300

    Menu padrão inicial

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,7 +1,29 @@
 <!-- need to remove -->
 <li class="nav-item">
-    <a href="{{ route('home') }}" class="nav-link active">
+    <a href="{{ route('home') }}" class="nav-link {{ (request()->is('home*')) ? 'active' : '' }}">
         <i class="nav-icon fas fa-home"></i>
         <p>Home</p>
     </a>
 </li>
+<!--@can('fornecedor-list')
+<li class="nav-item">
+    <a href="{{ route('fornecedores.index') }}" class="nav-link {{ (request()->is('fornecedores*')) ? 'active' : '' }}">
+        <i class="nav-icon fas fa-university"></i>
+        <p>Fornecedores</p>
+    </a>
+</li>
+@endcan-->
+@role('Super Admin')
+<li class="nav-item">
+    <a href="{{ route('users.index') }}" class="nav-link {{ (request()->is('users*')) ? 'active' : '' }}">
+        <i class="nav-icon fas fa-user"></i>
+        <p>Usuários</p>
+    </a>
+</li>
+<li class="nav-item">
+    <a href="{{ route('roles.index') }}" class="nav-link {{ (request()->is('roles*')) ? 'active' : '' }}">
+        <i class="nav-icon fas fa-lock"></i>
+        <p>Perfis</p>
+    </a>
+</li>
+@endcan
