467404223e047009fd3ced5e4f658d16b4ec3409
Guilherme Dambros <guilherme.dambros@gmail.com>
2021-09-10 13:30:54 -0300

    email confirmação pt_br

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Support\ServiceProvider;
 use Spatie\Permission\Models\Role;
 use App\Models\User;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -33,5 +35,12 @@ class AppServiceProvider extends ServiceProvider
         }); */
 
         Paginator::useBootstrap();
+		
+		VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage)
+                ->subject('wwwVerify Email Address')
+                ->line('Click the button below to verify your email address.')
+                ->action('Verify Email Address', $url);
+        });
     }
 }
